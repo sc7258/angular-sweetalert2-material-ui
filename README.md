21d0772ed1c664d1b6cb78d424493eb3460b95f2
sc7258 <sc7258@gmail.com>
2021-09-17 15:08:17 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-scss-demo-wm54oy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-scss-demo-wm54oy)
---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+//import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  name = 'Angular 6';
+
+  onClick() {
+    Swal.fire('', 'That thing is still around?');
+  }
+}
